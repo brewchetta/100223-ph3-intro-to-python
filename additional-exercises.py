5616eef9cabfa0bd33f547da01ad5877859acd3e
brewchetta <chett.tiller@gmail.com>
2023-11-13 13:41:19 -0500

    a man a plan a canal panama

--- additional-exercises.py
+++ additional-exercises.py
@@ -8,6 +8,9 @@
 
 # It returns the number squared.
 
+def square_number(number):
+    return number ** 2
+
 ##### sum_list #####
 
 # Create a function called 'sum_list'. 
@@ -15,6 +18,14 @@
 # It accepts a list of numbers as an argument (for example [1,2,3]). 
 
 # It returns the sum of all the numbers inside the list (the return from the example would be 6).
+
+def sum_list(list):
+    total = 0
+
+    for num in list:
+        total += num
+
+    return total
 
 ##### farenheit_to_celcius #####
 
@@ -26,6 +37,9 @@
 
 # If the number is a float, round down to the nearest whole number.
 
+def farenheit_to_celcius(temp):
+    return int( (temp - 32) * (5/9) )
+
 ##### is_planet #####
 
 # Create a function called 'is_planet'. 
@@ -36,6 +50,13 @@
 
 # This ought to work regardless of capitalization and whitespace.
 
+def is_planet(string):
+    string = string.title().strip()
+    
+    planets = ["Mercury", "Venus", "Earth", "Mars", "Tattooine"]
+
+    return string in [ pl.title() for pl in planets ]
+
 ##### is_palindrome #####
 
 # Create a function called 'is_palindrome'. 
@@ -45,3 +66,7 @@
 # If the string is a palindrome (it's the same forward as backwards) then return True, otherwise return False. An example of a palindrome is 'abba' or 'radar'.
 
 # As an additional challenge the function ignores white space so something like 'taco cat' would also be considered a palindrome.
+
+def is_palindrome(string):
+    modified_str = string.replace(" ", "").lower()
+    return modified_str == modified_str[::-1]
